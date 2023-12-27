5175311360ee4ae65c2ecd89505a9de14cba6686
Oxy0q <s1186487@student.windesheim.nl>
2023-12-27 14:16:49 +0100

    Footer changed

--- productpage/review.php
+++ productpage/review.php
@@ -2,24 +2,69 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "review";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
-// Check connection
+$db = "reviews";
+$conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully <br>";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $review = $_POST['review'];
+    $review = mysqli_real_escape_string($conn, $_POST['review']);
 
-    echo "Review submitted successfully!";
-} else {
-    echo "Error: " . "<br>" . mysqli_error($conn);
+    $sql = "INSERT INTO review (review1) VALUES ('$review')";
+    if (mysqli_query($conn, $sql)) {
+        echo "";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 }
 
-$conn->close();
+$result = mysqli_query($conn, "SELECT * FROM review");
+$reviews = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $reviews[] = $row['review1'];
+}
+
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="review-page.css">
+    <meta charset="UTF-8">
+    <title>Review Page</title>
+</head>
+<body>
+<div class="review-box">
+    <form action="" method="post">
+        <label for="review"></label>
+        <textarea id="review" name="review" class="review-box"></textarea>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
+
+<div class="body">
+    <p>reviews:</p>
+    <div class="divider"></div>
+
+    <?php
+    // Display reviews
+    foreach ($reviews as $review) {
+        echo "<p>$review</p>";
+    }
+    ?>
+</div>
+
+<style>
+    body {
+        background-color: lightskyblue;
+    }
+</style>
+</body>
+</html>
+
 
 
 
